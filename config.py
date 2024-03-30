4ee382a91bf53692efabeed94861dbaa6aae4d7b
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-03-30 07:24:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ GIT_TOKEN = getenv(
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHIVANSH474")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mastiwithfriendsxd")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
