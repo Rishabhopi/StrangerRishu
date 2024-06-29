bc6292cffc150c70695cc70c2ed9d84282c257d7
˹sᴛʀᴀɴɢᴇʀ-ꭙᴅ⁹⁹˼  🇮🇳 <147219905+itzshukla@users.noreply.github.com>
2024-06-29 22:49:01 +0530

    Update __main__.py

--- SHUKLAMUSIC/__main__.py
+++ SHUKLAMUSIC/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await SHUKLA.decorators()
     LOGGER("SHUKLAMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛 𝗦𝗛𝗨𝗞𝗟𝗔☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
