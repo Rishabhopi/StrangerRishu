ce4be03dfaa8f7a7aedc7048fa8a6a8598387d34
˹sᴛʀᴀɴɢᴇʀ-ꭙᴅ⁹⁹˼  🇮🇳 <147219905+itzshukla@users.noreply.github.com>
2024-07-08 08:55:39 +0530

    Update ping.py

--- SHUKLAMUSIC/plugins/tools/ping.py
+++ SHUKLAMUSIC/plugins/tools/ping.py
@@ -12,7 +12,7 @@ from SHUKLAMUSIC.utils.inline import supp_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
