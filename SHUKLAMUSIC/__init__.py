53ece9c5b074b99bda874d4c1df20b918ca7ac9c
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-08-23 13:55:06 +0530

    Update __init__.py

--- SHUKLAMUSIC/__init__.py
+++ SHUKLAMUSIC/__init__.py
@@ -27,4 +27,4 @@ Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
 
-APP = "ll_DRAGON_MUSIC_BOT"  # connect music api key "Dont change it"
+APP = "VIP_MUSIC_VC_BOT"  # connect music api key "Dont change it"
