0e7c87ac0309dcaf7365a984f88e937f3ce038f5
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-10 17:41:01 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="ᴏᴡɴᴇʀ", user_id=config.OWNER2_ID),
+         InlineKeyboardButton(text="ᴏᴡɴᴇʀ", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
